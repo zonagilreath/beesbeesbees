09f9c7f3b2a815148c19784c33cc1e003607d933
zonagilreath <bryson.gilreath@gmail.com>
2019-06-03 10:50:10 -0500

    Grub class implemented, passing tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,9 @@
-var Grub = function() {
+var Grub = function(age, color, food) {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(){
+  return `I eat ${this.food}`;
+}
